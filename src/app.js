7d9d990e6fb7dafc7108eb170b07dae49c5965b1
MMLaw95 <103419849+MMLaw95@users.noreply.github.com>
2022-06-06 23:54:07 -0500

    Remove old forecast structure html js

--- src/app.js
+++ src/app.js
@@ -58,8 +58,8 @@ function displayWeatherConditions(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-  minElement.innerHTML = Math.round(response.data.main.temp_min);
-  maxElement.innerHTML = Math.round(response.data.main.temp_max);
+  // minElement.innerHTML = Math.round(response.data.main.temp_min);
+  // maxElement.innerHTML = Math.round(response.data.main.temp_max);
 }
 
 function search(city) {
